af47573022f09b461c339299b399588fed7d05dd
gitnitinhub <nitin7817@gmail.com>
2022-09-30 01:25:13 +0530

    footer completed

---
+++ components/footer_imp.js
@@ -0,0 +1,50 @@
+const footer = () => {
+    return ` <div id="footer_logo_container">
+    <div id="footer_logo">
+        <img src="https://storage.googleapis.com/lumen5-site-images/L5-logo/L5-logo-white.png"/>
+        <p>Lumen5 combines powerful A.I. with a simple drag-and-drop interface to help you create professional video content in minutes.</p>
+    </div>
+   </div>
+    
+    <div id="footer_list_container">
+        <div id="footer_list_inside_conatiner">
+            <div>
+                <h3>EXPORE</h3>
+                <ul>
+                    <li><a href=#>About</a></li><br>
+                    <li><a href=#>Feature</a></li><br>
+                    <li><a href=#>Careers</a></li><br>
+                    <li><a href=#>Linkedln</a></li><br>
+                    <li><a href=#>Support</a></li><br>
+                    <li><a href=#>Product update</a></li><br>
+                </ul>
+            </div>
+            <div>
+                <h3>VIDEO TOOLS</h3>
+                <ul>
+                    <li><a href=#>Add Text to Video</a></li><br>
+                    <li><a href=#>Cut Video</a></li><br>
+                    <li><a href=#>Merge Video</a></li><br>
+                    <li><a href=#>Compress Video</a></li><br>
+                    <li><a href=#>Resize Video</a></li><br>
+                    <li><a href=#>See all</a></li><br>
+                </ul>
+            </div>
+            <div>
+                <h3>VIDEO CREATORS</h3>
+                <ul>
+                    <li><a href=#>Video Editor</a></li><br>
+                    <li><a href=#>Photo Video Maker</a></li><br>
+                    <li><a href=#>Facebook Video Maker</a></li><br>
+                    <li><a href=#>Youtube Intro Maker</a></li><br>
+                    <li><a href=#>Instagram Video Maker</a></li><br>
+                    <li><a href=#>See all</a></li><br>
+                </ul>
+            </div>
+        </div>
+    </div>
+ </div>
+ `
+};
+
+export default footer;
---
+++ footer.js
@@ -0,0 +1,10 @@
+import navbar from './components/navbar_imp.js';
+
+let navbar_div = document.getElementById('navbar');
+navbar_div.innerHTML = navbar();
+
+
+import footer from './components/footer_imp.js';
+
+let footer_div = document.getElementById('footer');
+footer_div.innerHTML = footer();

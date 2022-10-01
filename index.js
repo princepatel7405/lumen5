19fecfaded4f4faa9b56e66010f474dcf5cb5f91
Kuldeep Kaur <kaur24kuldeep@gmail.com>
2022-10-01 11:10:02 +0530

    homePage

--- index.js
+++ index.js
@@ -20,13 +20,26 @@ const btn_name = [
     { name : "Localization",},
 ]
 
-const videos = [
-    {video : "https://storage.googleapis.com/lumen5-site-images/homepage/Content%20Marketing/Hydrogen%20Economy.mp4", id: 1,},
-    {video : "https://storage.googleapis.com/lumen5-site-images/homepage/Content%20Marketing/siemens-content.mp4", id:2,},
-    {video : "https://storage.googleapis.com/lumen5-site-images/homepage/Content%20Marketing/Responsible%20Investment.mp4", id:3},
-    {video : "https://storage.googleapis.com/lumen5-site-images/homepage/Content%20Marketing/Workplace%20Evolution.mp4", id:4},
-    {video : "https://storage.googleapis.com/lumen5-site-images/homepage/Content%20Marketing/The%20Pandemic.mp4", id:5},
-    {video : "https://storage.googleapis.com/lumen5-site-images/homepage/Content%20Marketing/Supply%20Chain%20Resilience.mp4", id:6},
+const videos_contentMarket = [
+    {video : "https://storage.googleapis.com/lumen5-site-images/homepage/Content%20Marketing/Hydrogen%20Economy.mp4",},
+    {video : "https://storage.googleapis.com/lumen5-site-images/homepage/Content%20Marketing/siemens-content.mp4",},
+    {video : "https://storage.googleapis.com/lumen5-site-images/homepage/Content%20Marketing/Responsible%20Investment.mp4",},
+    {video : "https://storage.googleapis.com/lumen5-site-images/homepage/Content%20Marketing/Workplace%20Evolution.mp4",},
+    {video : "https://storage.googleapis.com/lumen5-site-images/homepage/Content%20Marketing/The%20Pandemic.mp4",},
+    {video : "https://storage.googleapis.com/lumen5-site-images/homepage/Content%20Marketing/Supply%20Chain%20Resilience.mp4",},
+];
+
+const videos_communications = [
+    {video : "https://storage.googleapis.com/lumen5-site-images/prev_tracer.mp4",},
+    {video : "https://storage.googleapis.com/lumen5-site-images/prev_billow.mp4",},
+    {video : "https://storage.googleapis.com/lumen5-site-images/prev_circuit.mp4",},
+    {video : "https://storage.googleapis.com/lumen5-site-images/prev_vertex.mp4",},
+    {video : "https://storage.googleapis.com/lumen5-site-images/prev_circuit.mp4",},
+    {video : "https://storage.googleapis.com/lumen5-site-images/prev_notice.mp4",},
+
+    {video : "https://storage.googleapis.com/lumen5-site-images/flux_prev.mp4",},
+    {video : "https://storage.googleapis.com/lumen5-site-images/unity_prev.mp4",},
+    {video : "https://storage.googleapis.com/lumen5-site-images/looper_prev.mp4",},
 ];
 
 
@@ -43,18 +56,18 @@ const append = (btn_data) => {
 
     let btn = document.createElement("button");
         btn.innerHTML = el.name;
-        btn.addEventListener("mouseover", (event) =>{
-            btn.style.background = "#2E86C1";
-            btn.style.padding = "20px"
-            btn.style.color = "white";
-        });
+        // btn.addEventListener("mouseover", (event) =>{
+        //     btn.style.background = "#2E86C1";
+        //     btn.style.padding = "20px"
+        //     btn.style.color = "white";
+        // });
 
-        btn.addEventListener("mouseout", (event) =>{
-            btn.style.background = "white";
-            // btn.style.border = "#2E86C1";
-            btn.style.padding = "10px"
-            btn.style.color = "#2E86C1";
-        });
+        // btn.addEventListener("mouseout", (event) =>{
+        //     btn.style.background = "white";
+        //     btn.style.border = "1px solid #2E86C1";
+        //     btn.style.padding = "10px"
+        //     btn.style.color = "#2E86C1";
+        // });
 
         data_div.append(btn);
     });
@@ -68,14 +81,18 @@ const append_video = (data) => {
     
    //data_div.innerHTML = null;
 
-    data.forEach((el) => {
+   data.forEach((el) => {
         
         let div = document.createElement("div");
 
         let iframe = document.createElement('iframe');
         iframe.src = el.video;
 
+        // iframe.setAttribute("autoplay=1");
+        // iframe.setAttribute("mute", true);
+        // iframe.muted;
 
+        
         // img.addEventListener('mouseover', (event) => {
         //     img.style.width = "105%";
         //     img.style.position = "relative";
@@ -92,4 +109,26 @@ const append_video = (data) => {
     });
 };
 
-append_video(videos);
+append_video(videos_contentMarket);
+
+const append_videos = (data) => {
+    let data_divv = document.getElementById("video_template");
+    
+   //data_div.innerHTML = null;
+
+   data.forEach((el) => {
+        
+        let div = document.createElement("div");
+
+        let iframe = document.createElement('iframe');
+        iframe.src = el.video;
+
+
+        div.append(iframe);
+
+        data_divv.append(div);
+
+    });
+};
+
+append_videos(videos_communications);
